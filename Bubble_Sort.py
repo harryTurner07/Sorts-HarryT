90ae0dddc9547b05d9789874b4a356d659a5173f
harryTurner07 <19122140@hlnsc.ac.uk>
2023-11-23 10:42:03 +0000

    Fully done the bubble sort

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -2,16 +2,16 @@ def bubble_sort(items):
     """
     A sorting algorithm using bubbles, it goes through the list bubbling the highest numbers to the top.
     """
-    l = len(items) # Set l to be the length of the list
+    length = len(items) # Set length to be the length of the list
     swapped = True # Set swapped to be True for the loop
-    l = int(l) - 1 # For some reason this line is needed to keep it in range :/
     while swapped:
         swapped = False # Reset at the start of each iteration
-        for i in range(l):
+        for i in range(length - 1):
             if items[i] > items[i+1]:
                 items[i], items[i+1] = items[i+1], items[i]
+                # ^ if the items before is greater than the item next to it, swap them
                 swapped = True
     return items
 
-item_list = [5,7,2,7,1,4,3]
+item_list = [5,7,2,7,1,4,3] # Items
 print(bubble_sort(item_list))
