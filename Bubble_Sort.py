62e7f97981678a9b27b96e3dcef56147a55c7a9c
harryTurner07 <19122140@hlnsc.ac.uk>
2023-11-23 11:14:04 +0000

    suffering :)

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -11,15 +11,15 @@ def bubble_sort(items):
                 items[i], items[i+1] = items[i+1], items[i]
                 # ^ if the items before is greater than the item next to it, swap them
                 swapped = True
-        if not swapped: # no swapping means the list is sorted
-            break # so no need for further comparison
+        length -= 1 # Should hopefully decrease the length everytime the swap is made
+        # ^ Using a test on replit, this line made it faster more consistently - avg time ~50 to 60 ms
+        # But without the line - avg time ~100+ ms
     return items
 
 item_list = [5,7,2,7,1,4,3] # Items
 print(bubble_sort(item_list))
 # Extra code
 """
-length -= 1 # Should hopefully decrease the length everytime the swap is made
-        # ^ Using a test on replit, this line made it faster more consistently - avg time ~50 to 60 ms
-        # But without the line - avg time ~100+ ms
+if not swapped: # no swapping means the list is sorted
+            break # so no need for further comparison
 """
