00b0e4874ac5c0bb78d56b47cf7eb1a39a8e33f0
harryTurner07 <146332725+harryTurner07@users.noreply.github.com>
2023-11-23 10:14:03 +0000

    Added the bubble sort
    Added the bubble sort to the main github link

---
+++ Bubble_Sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(items, search_item):
+    n = len(items)
+    swapped = True # Initially set swapped to True
+    while n > 0 and swapped:
+        swapped = False #Reset at start of each iteration
+        n = n - 1
+        pass
