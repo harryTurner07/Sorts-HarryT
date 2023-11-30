953477c8bb7ce5fcd339c04efaa2828f0f32bade
harryTurner07 <19122140@hlnsc.ac.uk>
2023-11-30 11:37:30 +0000

    Added a bunch of comments

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -6,16 +6,23 @@ def quick_sort(items):
     if len(items) <= 1:
         return items
     else:
+        # Just sets the variables to be used - here because if the length is 1 or less, not needed
         pivot_value = items[0]
         small_values = []
         large_values = []
         # Recursive Case
         for i in range(1,len(items)):
             if items[i] < pivot_value:
-                small_values.append(items[i])
+                small_values.append(items[i]) # If the selected item is smaller than the pivot, add the INDEX OF I to the list of small values
             else:
-                large_values.append(items[i])
+                large_values.append(items[i]) # Same as small but this time, add/append to the big item list
         return quick_sort(small_values) + [pivot_value] + quick_sort(large_values)
+        # Return the small list, the original pivot item, and the large list all as one big big list
 
 list_of_items = [2,5,1,6,4,3]
 print(quick_sort(list_of_items))
+
+# Bug(s)
+# Items being appended wrong
+# ^ Fixed - lines were the value and not index
+# ^ e.g. append(i) when it should have been append(items[i])
