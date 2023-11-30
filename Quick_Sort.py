2f8d3f7fe3aa5f6aca061454c978165f3de70a83
harryTurner07 <19122140@hlnsc.ac.uk>
2023-11-30 11:33:08 +0000

    Fixed the items appending

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -2,20 +2,20 @@ def quick_sort(items):
     """
     A quick sort is a sorting algorithm that uses pivots to order the items in a list
     """
-    length = len(items) # Im not writing this over and over
     # Base Case
-    if length <= 1:
+    if len(items) <= 1:
         return items
     else:
         pivot_value = items[0]
         small_values = []
         large_values = []
         # Recursive Case
-        for i in range(length):
+        for i in range(1,len(items)):
             if items[i] < pivot_value:
-                small_values.append(i)
+                small_values.append(items[i])
             else:
-                large_values.append(i)
-            return quick_sort(small_values) + [pivot_value] + quick_sort(large_values)
+                large_values.append(items[i])
+        return quick_sort(small_values) + [pivot_value] + quick_sort(large_values)
 
-list_of_items = [9,6,3,8,1,5,4,2,7,10]
+list_of_items = [2,5,1,6,4,3]
+print(quick_sort(list_of_items))
