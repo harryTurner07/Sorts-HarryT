5ffac494553dac551819cef3d16d1b395606a0b3
harryTurner07 <19122140@hlnsc.ac.uk>
2023-12-01 15:32:42 +0000

    Work In Progress

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,5 +1,16 @@
+"""
+For this merge sort I've got an idea on how to implement it but it's quite difficult
+So in the meanwhile the actual merge code will be on Merge_Sort_2.py
+And I'll actually finish this later. 
+"""
+
 def merge_sort(items):
-    pass
+    sorted_list = []
+    for i in range(len(items)):
+        example_list = []
+        example_list.append(items[i])
+    return example_list
+    
 
 list_of_items = [9,1,8,2,7,3,6,4,5]
 print(merge_sort(list_of_items))
