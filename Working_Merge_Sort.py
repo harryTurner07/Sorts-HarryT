90f72831d909c9635f2ffa8d7109faeeeb90b48c
harryTurner07 <19122140@hlnsc.ac.uk>
2023-12-01 15:45:41 +0000

    Did code things, still broke

---
+++ Working_Merge_Sort.py
@@ -0,0 +1,23 @@
+def merge_sort(list_a, list_b):
+    a = 0
+    b = 0
+    sorted_list = []
+    while a < len(list_a) and j < len(list_b):
+        first = list_a[a]
+        second = list_b[b]
+        if first < second:
+            #do stuff
+            pass
+        else:
+            #do other stuff
+            pass
+        #now, add the rest of the sublist with items left into the combined list
+        if True: # first list still has stuff in, add it all to the sorted list in the corrected positions
+            pass
+        else:
+            pass
+        return sorted_list
+
+list_a = [1,5,2,4,3]
+list_b = [6,10,7,9,8]
+print(merge_sort(list_a, list_b))
