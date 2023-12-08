af236ce2690e4288022e1c7fc12f1338ae9db013
harryTurner07 <19122140@hlnsc.ac.uk>
2023-12-08 14:55:03 +0000

    Made some comments

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -4,14 +4,14 @@ def insertion_sort(items):
     With every new item in a list, it sorts it ready for the next item -
     it doesn't move on until the past item and the current list is sorted
     """
-    for i in range(len(items)):
-        current_item = items[i]
+    for i in range(len(items)): # Does this for every item in the list
+        current_item = items[i] # Sets the current item in the list to the index
         new_index = i
         while (new_index > 0) and (items[new_index - 1] > current_item): # correct position not found
             items[new_index] = items[new_index - 1] # shuffle item accross
             new_index -= 1
         items[new_index] = current_item # Places item to new position
-    return items
+    return items # When the list is sorted, return the items
 
 list_of_items = [1,9,2,8,3,7,4,6,5]
 print(insertion_sort(list_of_items))
