50517ccb062126659a00c207c44e6868a5d15fc8
harryTurner07 <19122140@hlnsc.ac.uk>
2023-12-08 14:53:25 +0000

    Made the code

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,2 +1,17 @@
 def insertion_sort(items):
-    pass
+    """
+    An insertion sort is similar to a bubble sort, but it works one item at a time
+    With every new item in a list, it sorts it ready for the next item -
+    it doesn't move on until the past item and the current list is sorted
+    """
+    for i in range(len(items)):
+        current_item = items[i]
+        new_index = i
+        while (new_index > 0) and (items[new_index - 1] > current_item): # correct position not found
+            items[new_index] = items[new_index - 1] # shuffle item accross
+            new_index -= 1
+        items[new_index] = current_item # Places item to new position
+    return items
+
+list_of_items = [1,9,2,8,3,7,4,6,5]
+print(insertion_sort(list_of_items))
